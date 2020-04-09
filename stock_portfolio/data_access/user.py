95704ee8fe8bf7c3d804987377c2ddc4fa0a1a18
gaurav <gaurav@yelp.com>
2020-04-08 20:23:15 -0700

    Fix login logic for POST requests

--- stock_portfolio/data_access/user.py
+++ stock_portfolio/data_access/user.py
@@ -6,3 +6,11 @@ def register_user(
 ):
     """Store user information in the database"""
     return username
+
+
+def get_user(
+    username: str,
+    password: str,
+):
+    """Get user information from the database"""
+    return username
--- stock_portfolio/util/auth.py
+++ stock_portfolio/util/auth.py
@@ -1,6 +1,6 @@
-from flask import check_password_hash
+from werkzeug.security import check_password_hash
 
-from stock_portfolio.data_access.user import get_user_info
+from stock_portfolio.data_access.user import get_user
 
 
 class InvalidLoginCredentialsException(Exception):
@@ -13,7 +13,7 @@ def validate_login_credentials(
 ):
     # Retrieve the user's information and set the user id in the
     # session if the login credentials are correct
-    user = get_user_info(username, password)
+    user = get_user(username, password)
     if not user.username == username or not check_password_hash(
         user.password,
         password,
