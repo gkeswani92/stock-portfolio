2346f59b92507d71e154ba991e1b4ea2b2a4f3d0
gaurav <gaurav@yelp.com>
2020-04-08 20:16:46 -0700

    Adding login logic for POST request

---
+++ stock_portfolio/util/auth.py
@@ -0,0 +1,21 @@
+from flask import check_password_hash
+
+from stock_portfolio.data_access.user import get_user_info
+
+
+class InvalidLoginCredentialsException(Exception):
+    pass
+
+
+def validate_login_credentials(
+    username: str,
+    password: str,
+):
+    # Retrieve the user's information and set the user id in the
+    # session if the login credentials are correct
+    user = get_user_info(username, password)
+    if not user.username == username or not check_password_hash(
+        user.password,
+        password,
+    ):
+        raise InvalidLoginCredentialsException
--- stock_portfolio/views/auth.py
+++ stock_portfolio/views/auth.py
@@ -1,10 +1,17 @@
 from flask import Blueprint
+from flask import flash
 from flask import render_template
+from flask import redirect
 from flask import request
+from flask import session
+from flask import url_for
 from werkzeug.exceptions import BadRequest
 from werkzeug.exceptions import InternalServerError
 
 from stock_portfolio.data_access.user import register_user
+from stock_portfolio.data_access.user import register_user
+from stock_portfolio.util.auth import InvalidLoginCredentialsException
+from stock_portfolio.util.auth import validate_login_credentials
 
 # Defining a blue print for all URLs that begin with /auth.
 # All views that are related to auth should be registered with
@@ -46,4 +53,19 @@ def login():
     if request.method == 'GET':
         return render_template('auth/login.html')
     else:
-        pass
+        username = request.form.get('username')
+        password = request.form.get('password')
+        if not username or not password:
+            raise BadRequest('Username and password are required fields')
+
+        # Retrieve the user's information and set the user id in the
+        # session if the login credentials are correct
+        try:
+            user = validate_login_credentials(username, password)
+        except InvalidLoginCredentialsException:
+            flash('Invalid Login Credentials')
+            return
+        else:
+            session.clear()
+            session.user['user_id'] = user.id
+            return redirect(url_for('index'))
