e4c270fa3973010ce05f5ccf4d567e1582441881
gkeswani92 <gauravkeswani92@gmail.com>
2020-05-09 15:46:33 -0700

    Add controller to render the portfolio for a specific provider

--- stock_portfolio/views/portfolio.py
+++ stock_portfolio/views/portfolio.py
@@ -1,6 +1,13 @@
+from typing import Set
+
+from flask import abort
 from flask import Blueprint
 from flask import render_template
 
+SUPPORTED_PROVIDERS: Set[str] = {
+    'Fidelity',
+    'Robinhood',
+}
 
 # Defining a blue print for all URLs that begin with /portfolio.
 # All views that are related to portfolio should be registered with
@@ -11,4 +18,21 @@ portfolio_bp = Blueprint('portfolio_bp', __name__, url_prefix='/portfolio')
 
 @portfolio_bp.route('/')
 def index():
-    return render_template('portfolio/home.html')
+    return render_template(
+        'portfolio/home.html',
+        supported_providers=SUPPORTED_PROVIDERS,
+    )
+
+
+# The <string: provider> in the route means we are expecting an string after
+# /portfolio and all routes like /portfolio/1, /portfolio/2 should be routed
+# to this controller
+@portfolio_bp.route('/<string:provider>')
+def portfolio(provider: str):
+    if provider not in SUPPORTED_PROVIDERS:
+        abort(404)
+
+    return render_template(
+        'portfolio/home.html',
+        provider=provider.capitalize(),
+    )
