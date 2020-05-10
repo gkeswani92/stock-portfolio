26efcf59f5a2a87fad6b2535f9f403d4c8019a07
gkeswani92 <gauravkeswani92@gmail.com>
2020-05-10 12:07:48 -0700

    Add validators to the register form

--- stock_portfolio/forms/register.py
+++ stock_portfolio/forms/register.py
@@ -1,9 +1,29 @@
 from flask_wtf import FlaskForm
 from wtforms import SubmitField
 from wtforms import StringField
+from wtforms.validators import DataRequired
+from wtforms.validators import InputRequired
+from wtforms.validators import Length
 
 
 class RegisterUserForm(FlaskForm):
-    username = StringField('Username')
-    password = StringField('Password')
+    username = StringField(
+        'Username',
+        validators=[
+            InputRequired('Username is required!'),
+            DataRequired('Username is required!'),
+            Length(
+                min=5,
+                max=20,
+                message='Username must be between 5 and 20 characters',
+            ),
+        ]
+    )
+    password = StringField(
+        'Password',
+        validators=[
+            InputRequired('Username is required!'),
+            DataRequired('Username is required!'),
+        ]
+    )
     submit = SubmitField('Register')
