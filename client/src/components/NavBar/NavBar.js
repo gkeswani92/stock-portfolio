7de8b41d2757909650a2b179134472bc1822a06d
gkeswani92 <gauravkeswani92@gmail.com>
2020-05-13 23:24:27 -0700

    Add CSS to register page for XS container

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,25 +1,34 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./NavBar.css";
 
 export default class NavBar extends Component {
-    render() {
-        return (
-            <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to='/home'>Home</Link>
-                        </li>
-                        <li>
-                            <Link to='/login'>Login</Link>
-                        </li>
-                        <li>
-                            <Link to='/register'>Register</Link>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <nav class="nav navbar justify-content-left navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand" href="#">
+              <img
+                src="/docs/4.1/assets/brand/bootstrap-solid.svg"
+                width="30"
+                height="30"
+                alt=""
+              />
+              Portfolio Tracker
+            </a>
+            <Link to="/home" class="nav-item nav-link text-light">
+              Home
+            </Link>
+
+            <Link to="/login" class="nav-item nav-link text-light">
+              Login
+            </Link>
+
+            <Link to="/register" class="nav-item nav-link text-light active">
+              Register
+            </Link>
+        </nav>
+      </div>
+    );
+  }
 }
--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import "./Register.css";
 
 export default class Register extends Component {
   constructor() {
@@ -17,109 +18,125 @@ export default class Register extends Component {
 
   onChange(event) {
     this.setState({
-        [event.target.name]: event.target.value
+      [event.target.name]: event.target.value,
     });
   }
 
   onSubmit(event) {
     // We are using preventDefault so as to block the default browser action
     // of making a GET request to the backend
-    event.preventDefault()
+    event.preventDefault();
 
-    axios.post(
-        '/auth/register',
-        {
-            'first_name': this.state.first_name,
-            'last_name': this.state.last_name,
-            'username': this.state.username,
-            'password': this.state.password
-        }
-    ).then(response => {
-        console.log('Successfully registered ' + this.state.first_name)
-    })
+    axios
+      .post("/auth/register", {
+        first_name: this.state.first_name,
+        last_name: this.state.last_name,
+        username: this.state.username,
+        password: this.state.password,
+      })
+      .then((response) => {
+        console.log("Successfully registered " + this.state.first_name);
+      });
   }
 
   render() {
     return (
-      <div class="container-fluid">
-        <h1 class="mb-1 text-center">Sign up</h1>
-        <p class="fs-14 text-gray text-center mb-5">
-            Track your portfolio and see what your friends are up to!
-        </p>
-        <form onSubmit={this.onSubmit}>
+      <div class="signup_wrapper">
+        <div class="container container-xs">
+          <h2 class="mb-1 text-center">Sign up</h2>
+          <p class="fs-14 text-gray text-center mb-5">
+            Track your portfolio and share recommendations with friends.
+          </p>
+          <form onSubmit={this.onSubmit}>
             <div class="form-row col-auto">
-                <div class="form-group col-md-6">
-                    <label htmlFor="first_name" class="col-form-label-lg">First Name</label>
-                    <input
-                        type="text"
-                        name="first_name"
-                        value={this.state.first_name}
-                        onChange={this.onChange}
-                        placeholder="Enter First Name"
-                        class="form-control"
-                        required
-                    />
-                </div>
-
-                <div class="form-group col-md-6">
-                    <label htmlFor="last_name" class="col-form-label-lg">Last Name</label>
-                    <input
-                        type="text"
-                        name="last_name"
-                        value={this.state.last_name}
-                        onChange={this.onChange}
-                        placeholder="Enter Last Name"
-                        class="form-control"
-                        required
-                    />
-                </div>
-            </div>
-
-            <div class="form-group col-auto">
-                <label htmlFor="username" class="col-form-label-lg">Username</label>
-                <div class="input-group">
-                    <div class="input-group-prepend">
-                        <span class="input-group-text" id="inputGroupPrepend2">@</span>
-                    </div>
-                    <input
-                        type="text"
-                        name="username"
-                        value={this.state.username}
-                        onChange={this.onChange}
-                        placeholder="Enter Username"
-                        class="form-control"
-                        required
-                    />
-                </div>
-            </div>
-
-            <div class="form-group col-auto">
-                <label htmlFor="password" class="col-form-label-lg">Password</label>
+              <div class="form-group col-md-6">
+                <label htmlFor="first_name" class="col-form-label-sm">
+                  First Name
+                  <span class="required"> *</span>
+                </label>
                 <input
-                    type="password"
-                    name="password"
-                    value={this.state.password}
-                    onChange={this.onChange}
-                    placeholder="Enter Password"
-                    class="form-control"
-                    required
+                  type="text"
+                  name="first_name"
+                  value={this.state.first_name}
+                  onChange={this.onChange}
+                  class="form-control"
+                  required
                 />
-                <small id="passwordHelpBlock" class="form-text text-muted">
-                    Your password must be 8-20 characters long
-                </small>
+              </div>
+
+              <div class="form-group col-md-6">
+                <label htmlFor="last_name" class="col-form-label-sm">
+                  Last Name
+                  <span class="required"> *</span>
+                </label>
+                <input
+                  type="text"
+                  name="last_name"
+                  value={this.state.last_name}
+                  onChange={this.onChange}
+                  class="form-control"
+                  required
+                />
+              </div>
             </div>
 
             <div class="form-group col-auto">
-                <button type="submit" class="btn btn-primary">Sign Up</button>
+              <label htmlFor="username" class="col-form-label-sm">
+                Username
+                <span class="required"> *</span>
+              </label>
+              <div class="input-group">
+                <div class="input-group-prepend">
+                  <span class="input-group-text" id="inputGroupPrepend2">
+                    @
+                  </span>
+                </div>
+                <input
+                  type="text"
+                  name="username"
+                  value={this.state.username}
+                  onChange={this.onChange}
+                  class="form-control"
+                  required
+                />
+              </div>
             </div>
-        </form>
 
-        <p class="text-gray-soft text-center small mb-2">
-            By clicking "Sign up" you agree to our <a href="https://themes.getbootstrap.com/terms">Terms of Service</a>.
-        </p>
-        <p class="text-gray-soft text-center small mb-2">
-            Already have an account? <a href="https://themes.getbootstrap.com/signin/">Sign in</a>
-        </p>
+            <div class="form-group col-auto">
+              <label htmlFor="password" class="col-form-label-sm">
+                Password
+                <span class="required"> *</span>
+              </label>
+              <input
+                type="password"
+                name="password"
+                value={this.state.password}
+                onChange={this.onChange}
+                class="form-control"
+                required
+              />
+              <small id="passwordHelpBlock" class="form-text text-muted">
+                Your password must be 8-20 characters long
+              </small>
+            </div>
+
+            <div class="form-group col-auto">
+              <button type="submit" class="btn btn-primary btn-block">
+                Sign Up
+              </button>
+            </div>
+          </form>
+
+          <p class="text-gray-soft text-center small mb-2">
+            By clicking "Sign up" you agree to our{" "}
+            <a href="https://themes.getbootstrap.com/terms">Terms of Service</a>
+            .
+          </p>
+          <p class="text-gray-soft text-center small mb-2">
+            Already have an account?{" "}
+            <a href="https://themes.getbootstrap.com/signin/">Sign in</a>
+          </p>
+        </div>
       </div>
     );
   }
