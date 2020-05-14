3b6ec29c2e47a414513c785496c0e58fc368d94e
gkeswani92 <gauravkeswani92@gmail.com>
2020-05-14 15:39:01 -0700

    Add NavBar to landing page

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,25 +1,36 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./NavBar.css";
 
 export default class NavBar extends Component {
-    render() {
-        return (
-            <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to='/home'>Home</Link>
-                        </li>
-                        <li>
-                            <Link to='/login'>Login</Link>
-                        </li>
-                        <li>
-                            <Link to='/register'>Register</Link>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <nav class="nav navbar justify-content-end navbar-expand-lg navbar-light">
+          <div class="container">
+            <a class="navbar-brand" href="#">
+              <img
+                src="/docs/4.1/assets/brand/bootstrap-solid.svg"
+                width="30"
+                height="30"
+                alt=""
+              />
+              Portfolio Tracker
+            </a>
+            <Link to="/home" class="nav-item nav-link text-secondary">
+              Home
+            </Link>
+
+            <Link to="/login" class="nav-item nav-link text-secondary">
+              Login
+            </Link>
+
+            <Link to="/register" class="nav-item nav-link text-secondary active">
+              Register
+            </Link>
+          </div>
+        </nav>
+      </div>
+    );
+  }
 }
