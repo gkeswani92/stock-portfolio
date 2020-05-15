d4254361f18da04f6c4a52e2c766174e46bde94d
gkeswani92 <gauravkeswani92@gmail.com>
2020-05-14 18:46:23 -0700

    Fix spacing between form label and input components

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -6,8 +6,7 @@ export default class NavBar extends Component {
   render() {
     return (
       <div>
-        <nav class="nav navbar justify-content-end navbar-expand-lg navbar-light">
-          <div class="container">
+        <nav class="nav navbar justify-content-left navbar-expand-lg navbar-dark bg-dark">
             <a class="navbar-brand" href="#">
               <img
                 src="/docs/4.1/assets/brand/bootstrap-solid.svg"
@@ -17,18 +16,17 @@ export default class NavBar extends Component {
               />
               Portfolio Tracker
             </a>
-            <Link to="/home" class="nav-item nav-link text-secondary">
+            <Link to="/home" class="nav-item nav-link text-light">
               Home
             </Link>
 
-            <Link to="/login" class="nav-item nav-link text-secondary">
+            <Link to="/login" class="nav-item nav-link text-light">
               Login
             </Link>
 
-            <Link to="/register" class="nav-item nav-link text-secondary active">
+            <Link to="/register" class="nav-item nav-link text-light active">
               Register
             </Link>
-          </div>
         </nav>
       </div>
     );
--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -41,99 +41,102 @@ export default class Register extends Component {
 
   render() {
     return (
-      <div class="container container-xs">
-        <h2 class="mb-1 text-center">Sign up</h2>
-        <p class="fs-14 text-gray text-center mb-5">
-          Track your portfolio and share recommendations with friends.
-        </p>
-        <form onSubmit={this.onSubmit}>
-          <div class="form-row col-auto">
-            <div class="form-group col-md-6">
-              <label htmlFor="first_name" class="col-form-label-sm">
-                First Name
-                <span class="required"> *</span>
-              </label>
-              <input
-                type="text"
-                name="first_name"
-                value={this.state.first_name}
-                onChange={this.onChange}
-                class="form-control"
-                required
-              />
-            </div>
-
-            <div class="form-group col-md-6">
-              <label htmlFor="last_name" class="col-form-label-sm">
-                Last Name
-                <span class="required"> *</span>
-              </label>
-              <input
-                type="text"
-                name="last_name"
-                value={this.state.last_name}
-                onChange={this.onChange}
-                class="form-control"
-                required
-              />
-            </div>
-          </div>
-
-          <div class="form-group col-auto">
-            <label htmlFor="username" class="col-form-label-sm">
-              Username
-              <span class="required"> *</span>
-            </label>
-            <div class="input-group">
-              <div class="input-group-prepend">
-                <span class="input-group-text" id="inputGroupPrepend2">
-                  @
-                </span>
+      <div class="signup_wrapper">
+        <div class="container container-xs">
+          <h2 class="mb-1 text-center">Sign up</h2>
+          <p class="fs-14 text-gray text-center mb-5">
+            Track your portfolio and share recommendations with friends.
+          </p>
+          <form onSubmit={this.onSubmit}>
+            <div class="form-row col-auto">
+              <div class="form-group col-md-6">
+                <label htmlFor="first_name" class="col-form-label-sm">
+                  First Name
+                  <span class="required"> *</span>
+                </label>
+                <input
+                  type="text"
+                  name="first_name"
+                  value={this.state.first_name}
+                  onChange={this.onChange}
+                  class="form-control"
+                  required
+                />
               </div>
+
+              <div class="form-group col-md-6">
+                <label htmlFor="last_name" class="col-form-label-sm">
+                  Last Name
+                  <span class="required"> *</span>
+                </label>
+                <input
+                  type="text"
+                  name="last_name"
+                  value={this.state.last_name}
+                  onChange={this.onChange}
+                  class="form-control"
+                  required
+                />
+              </div>
+            </div>
+
+            <div class="form-group col-auto">
+              <label htmlFor="username" class="col-form-label-sm">
+                Username
+                <span class="required"> *</span>
+              </label>
+              <div class="input-group">
+                <div class="input-group-prepend">
+                  <span class="input-group-text" id="inputGroupPrepend2">
+                    @
+                  </span>
+                </div>
+                <input
+                  type="text"
+                  name="username"
+                  value={this.state.username}
+                  onChange={this.onChange}
+                  class="form-control"
+                  required
+                />
+              </div>
+            </div>
+
+            <div class="form-group col-auto">
+              <label htmlFor="password" class="col-form-label-sm">
+                Password
+                <span class="required"> *</span>
+              </label>
               <input
-                type="text"
-                name="username"
-                value={this.state.username}
+                type="password"
+                name="password"
+                value={this.state.password}
                 onChange={this.onChange}
                 class="form-control"
                 required
               />
+              <small id="passwordHelpBlock" class="form-text text-muted">
+                Your password must be 8-20 characters long
+              </small>
             </div>
-          </div>
 
-          <div class="form-group col-auto">
-            <label htmlFor="password" class="col-form-label-sm">
-              Password
-              <span class="required"> *</span>
-            </label>
-            <input
-              type="password"
-              name="password"
-              value={this.state.password}
-              onChange={this.onChange}
-              class="form-control"
-              required
-            />
-            <small id="passwordHelpBlock" class="form-text text-muted">
-              Your password must be 8-20 characters long
-            </small>
-          </div>
+            <div class="form-group col-auto">
+              <button type="submit" class="btn btn-primary btn-block">
+                Sign Up
+              </button>
+            </div>
+          </form>
 
-          <div class="form-group col-auto">
-            <button type="submit" class="btn btn-primary btn-block">
-              Sign Up
-            </button>
-          </div>
-        </form>
-
-        <p class="text-gray-soft text-center small mb-2">
-          By clicking "Sign up" you agree to our{" "}
-          <a href="https://themes.getbootstrap.com/terms">Terms of Service</a>.
-        </p>
-        <p class="text-gray-soft text-center small mb-2">
-          Already have an account?{" "}
-          <a href="https://themes.getbootstrap.com/signin/">Sign in</a>
-        </p>
+          <p class="text-gray-soft text-center small mb-2">
+            By clicking "Sign up" you agree to our{" "}
+            <a href="https://themes.getbootstrap.com/terms">Terms of Service</a>
+            .
+          </p>
+          <p class="text-gray-soft text-center small mb-2">
+            Already have an account?{" "}
+            <a href="https://themes.getbootstrap.com/signin/">Sign in</a>
+          </p>
+        </div>
       </div>
     );
   }
