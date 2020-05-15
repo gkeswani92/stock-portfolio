4c044b5ce9df482556ecbb53e7811ee28a81590e
gkeswani92 <gauravkeswani92@gmail.com>
2020-05-14 23:25:19 -0700

    Bug fix for class to className

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -6,25 +6,25 @@ export default class NavBar extends Component {
   render() {
     return (
       <div>
-        <nav class="nav navbar justify-content-left navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand" href="#">
+        <nav className="nav navbar justify-content-left navbar-expand-lg navbar-dark bg-dark">
+            <a className="navbar-brand" href="#">
               <img
-                src="/docs/4.1/assets/brand/bootstrap-solid.svg"
+                src=""
                 width="30"
                 height="30"
                 alt=""
               />
               Portfolio Tracker
             </a>
-            <Link to="/home" class="nav-item nav-link text-light">
+            <Link to="/home" className="nav-item nav-link text-light">
               Home
             </Link>
 
-            <Link to="/login" class="nav-item nav-link text-light">
+            <Link to="/login" className="nav-item nav-link text-light">
               Login
             </Link>
 
-            <Link to="/register" class="nav-item nav-link text-light active">
+            <Link to="/register" className="nav-item nav-link text-light active">
               Register
             </Link>
         </nav>
--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -41,53 +41,53 @@ export default class Register extends Component {
 
   render() {
     return (
-      <div class="signup_wrapper">
-        <div class="container container-xs">
-          <h2 class="mb-1 text-center">Sign up</h2>
-          <p class="fs-14 text-gray text-center mb-5">
+      <div className="signup_wrapper">
+        <div className="container container-xs">
+          <h2 className="mb-1 text-center">Sign up</h2>
+          <p className="fs-14 text-gray text-center mb-4">
             Track your portfolio and share recommendations with friends.
           </p>
           <form onSubmit={this.onSubmit}>
-            <div class="form-row col-auto">
-              <div class="form-group col-md-6">
-                <label htmlFor="first_name" class="col-form-label-sm">
+            <div className="form-row col-auto">
+              <div className="form-group col-md-6">
+                <label htmlFor="first_name" className="col-form-label-sm">
                   First Name
-                  <span class="required"> *</span>
+                  <span className="required"> *</span>
                 </label>
                 <input
                   type="text"
                   name="first_name"
                   value={this.state.first_name}
                   onChange={this.onChange}
-                  class="form-control"
+                  className="form-control"
                   required
                 />
               </div>
 
-              <div class="form-group col-md-6">
-                <label htmlFor="last_name" class="col-form-label-sm">
+              <div className="form-group col-md-6">
+                <label htmlFor="last_name" className="col-form-label-sm">
                   Last Name
-                  <span class="required"> *</span>
+                  <span className="required"> *</span>
                 </label>
                 <input
                   type="text"
                   name="last_name"
                   value={this.state.last_name}
                   onChange={this.onChange}
-                  class="form-control"
+                  className="form-control"
                   required
                 />
               </div>
             </div>
 
-            <div class="form-group col-auto">
-              <label htmlFor="username" class="col-form-label-sm">
+            <div className="form-group col-auto">
+              <label htmlFor="username" className="col-form-label-sm">
                 Username
-                <span class="required"> *</span>
+                <span className="required"> *</span>
               </label>
-              <div class="input-group">
-                <div class="input-group-prepend">
-                  <span class="input-group-text" id="inputGroupPrepend2">
+              <div className="input-group">
+                <div className="input-group-prepend">
+                  <span className="input-group-text" id="inputGroupPrepend2">
                     @
                   </span>
                 </div>
@@ -96,43 +96,43 @@ export default class Register extends Component {
                   name="username"
                   value={this.state.username}
                   onChange={this.onChange}
-                  class="form-control"
-                  required
+                  className="form-control"
+                  required autoFocus
                 />
               </div>
             </div>
 
-            <div class="form-group col-auto">
-              <label htmlFor="password" class="col-form-label-sm">
+            <div className="form-group col-auto">
+              <label htmlFor="password" className="col-form-label-sm">
                 Password
-                <span class="required"> *</span>
+                <span className="required"> *</span>
               </label>
               <input
                 type="password"
                 name="password"
                 value={this.state.password}
                 onChange={this.onChange}
-                class="form-control"
+                className="form-control"
                 required
               />
-              <small id="passwordHelpBlock" class="form-text text-muted">
+              <small id="passwordHelpBlock" className="form-text text-muted">
                 Your password must be 8-20 characters long
               </small>
             </div>
 
-            <div class="form-group col-auto">
-              <button type="submit" class="btn btn-primary btn-block">
+            <div className="form-group col-auto">
+              <button type="submit" className="btn btn-primary btn-block">
                 Sign Up
               </button>
             </div>
           </form>
 
-          <p class="text-gray-soft text-center small mb-2">
+          <p className="text-gray-soft text-center small mb-2">
             By clicking "Sign up" you agree to our{" "}
             <a href="https://themes.getbootstrap.com/terms">Terms of Service</a>
             .
           </p>
-          <p class="text-gray-soft text-center small mb-2">
+          <p className="text-gray-soft text-center small mb-2">
             Already have an account?{" "}
             <a href="https://themes.getbootstrap.com/signin/">Sign in</a>
           </p>
