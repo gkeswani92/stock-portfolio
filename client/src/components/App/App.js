ce557e32f3cafcd11b3fc3e8c544056f78fab329
gkeswani92 <gauravkeswani92@gmail.com>
2020-05-15 20:47:33 -0700

    Create login page and server functionality

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -5,6 +5,7 @@ import {
 } from 'react-router-dom';
 import NavBar from '../NavBar/NavBar';
 import Register from '../Register/Register';
+import Login from '../Login/Login';
 import './App.css';
 
 function App() {
@@ -13,7 +14,7 @@ function App() {
       <div className="App">
         <NavBar />
 
-        <Route path='/login' />
+        <Route path='/login' component={Login} />
         <Route path='/register' component={Register} />
         <Route path='/home' />
 
