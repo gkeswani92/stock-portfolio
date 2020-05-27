5a4962341896b146ef5e1f82bfd261b15abe2b71
gkeswani92 <gauravkeswani92@gmail.com>
2020-05-26 23:21:14 -0700

    Convert transactions in feed to Media

--- client/src/components/Transaction/Transaction.js
+++ client/src/components/Transaction/Transaction.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import Row from "react-bootstrap/Row";
 import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
+import Media from "react-bootstrap/Media";
 import "./Transaction.css";
 
 export default class Transaction extends Component {
@@ -18,39 +19,30 @@ export default class Transaction extends Component {
 
   render() {
     return (
-      <Card>
-        <Row>
-          <div className="col-auto">
-            <Card.Img
-              src={"ticker/" + this.props.ticker + ".png"}
-              className="img-fluid ticker-image rounded-circle"
-              alt={this.props.ticker}
-            />
-          </div>
-          <Col>
-            <Card.Body>
-              {this.props.orderType === "BUY" && (
-                <h5 className="card-title buy-alert">Buy Alert</h5>
-              )}
-              {this.props.orderType === "SELL" && (
-                <h5 className="card-title sell-alert">Sell Alert</h5>
-              )}
+      <Media>
+        <img
+          src={"ticker/" + this.props.ticker + ".png"}
+          className="img-fluid ticker-image"
+          alt={this.props.ticker}
+        />
+        <Media.Body>
+          {this.props.orderType === "BUY" && (
+            <h5 className="buy-alert feed-content">Buy Alert</h5>
+          )}
+          {this.props.orderType === "SELL" && (
+            <h5 className="sell-alert feed-content">Sell Alert</h5>
+          )}
 
-              <Card.Text>
-                {this.props.firstName} {this.props.lastName}{" "}
-                {this.props.orderType === "BUY" && "bought"}{" "}
-                {this.props.orderType === "SELL" && "sold"}{" "}
-                {this.props.quantity} stocks of {this.props.ticker} at $
-                {this.props.price}
-              </Card.Text>
+          <p className="feed-content">
+            {this.props.firstName} {this.props.lastName}{" "}
+            {this.props.orderType === "BUY" && "bought"}{" "}
+            {this.props.orderType === "SELL" && "sold"} {this.props.quantity}{" "}
+            stocks of {this.props.ticker} at ${this.props.price}
+          </p>
 
-              <Card.Footer className="blockquote-footer">
-                {this.props.createdAt}
-              </Card.Footer>
-            </Card.Body>
-          </Col>
-        </Row>
-      </Card>
+          <footer className="blockquote-footer feed-content">{this.props.createdAt}</footer>
+        </Media.Body>
+      </Media>
     );
   }
 }
