09899a648bbe69fd20b3469eade34652dad937a2
gkeswani92 <gauravkeswani92@gmail.com>
2020-05-26 22:19:17 -0700

    Use react-bootstrap for  jumbotron

--- client/src/components/FollowUsersJumbotron/FollowUsersJumbotron.js
+++ client/src/components/FollowUsersJumbotron/FollowUsersJumbotron.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import Button from "react-bootstrap/Button";
+import Jumbotron from "react-bootstrap/Jumbotron";
 
 export default class FollowUsersJumbotron extends Component {
   static propTypes = {
@@ -8,19 +10,17 @@ export default class FollowUsersJumbotron extends Component {
 
   render() {
     return (
-      <div>
-        <div className="jumbotron">
-          <h1 className="display-5">Hello, {this.props.username}!</h1>
-          <p className="lead">
-            Please follow other users to keep track of their transactions.
-          </p>
-          <p className="lead">
-            <a className="btn btn-primary btn-lg" href="#" role="button">
-              Find users
-            </a>
-          </p>
-        </div>
-      </div>
+      <Jumbotron>
+        <h1 className="display-5">Hello, {this.props.username}!</h1>
+        <p className="lead">
+          Please follow other users to keep track of their transactions.
+        </p>
+        <p className="lead">
+          <Button variant="primary">
+            Find users
+          </Button>
+        </p>
+      </Jumbotron>
     );
   }
 }
