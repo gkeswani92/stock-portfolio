e403b22cccac263a190795a9c0c5f2702d5c1727
gkeswani92 <gauravkeswani92@gmail.com>
2020-05-29 19:21:14 -0700

    Clean up NavBar conditional logic for logged in v/s logged out

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -5,6 +5,32 @@ import Navbar from "react-bootstrap/Navbar";
 import "./NavBar.css";
 
 const NavBar = (props) => {
+  const loggedInLinks = (
+    <React.Fragment>
+      <Link to="/addTransaction" className="nav-item nav-link text-light">
+        Add Transaction
+      </Link>
+      <Link
+        to="/logout"
+        className="nav-item nav-link text-light"
+        onClick={() => props.handleLogout()}
+      >
+        Logout
+      </Link>
+    </React.Fragment>
+  );
+
+  const loggedOutLinks = (
+    <React.Fragment>
+      <Link to="/login" className="nav-item nav-link text-light">
+        Login
+      </Link>
+      <Link to="/register" className="nav-item nav-link text-light active">
+        Register
+      </Link>
+    </React.Fragment>
+  );
+
   return (
     <div>
       <Navbar bg="dark" variant="dark">
@@ -17,40 +43,11 @@ const NavBar = (props) => {
           Home
         </Link>
 
-        {/* Display the login and register button only if there is no user
-             that is currently logged in
-          */}
+        {/* Display the login and register button only if there is no user that is currently logged in */}
+        {!props.isLoggedIn && loggedOutLinks}
 
-        {!props.isLoggedIn && (
-          <Link to="/login" className="nav-item nav-link text-light">
-            Login
-          </Link>
-        )}
-
-        {!props.isLoggedIn && (
-          <Link to="/register" className="nav-item nav-link text-light active">
-            Register
-          </Link>
-        )}
-
-        {/* Display the logout button only if there is a user that is currently
-          logged in
-          */}
-        {props.isLoggedIn && (
-          <Link to="/addTransaction" className="nav-item nav-link text-light">
-            Add Transaction
-          </Link>
-        )}
-
-        {props.isLoggedIn && (
-          <Link
-            to="/logout"
-            className="nav-item nav-link text-light"
-            onClick={() => props.handleLogout()}
-          >
-            Logout
-          </Link>
-        )}
+        {/* Display the logout button only if there is a user that is currently logged in */}
+        {props.isLoggedIn && loggedInLinks}
       </Navbar>
     </div>
   );
