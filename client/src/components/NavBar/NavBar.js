9e4bafe3a77cc0c65f58742a744512f372b82bd3
gkeswani92 <gauravkeswani92@gmail.com>
2020-05-29 19:17:28 -0700

    Convert NavBar to a functional component

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -4,67 +4,57 @@ import { Link } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
 import "./NavBar.css";
 
-export default class NavBar extends Component {
-  onLogoutClick = () => {
-    console.log("Logout button clicked");
-    this.props.handleLogout();
-  };
+const NavBar = (props) => {
+  return (
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="/home">
+          <img src="" width="30" height="30" alt="" />
+          Portfolio Tracker
+        </Navbar.Brand>
 
-  render() {
-    return (
-      <div>
-        <Navbar bg="dark" variant="dark">
-          <Navbar.Brand href="/home">
-            <img src="" width="30" height="30" alt="" />
-            Portfolio Tracker
-          </Navbar.Brand>
+        <Link to="/home" className="nav-item nav-link text-light">
+          Home
+        </Link>
 
-          <Link to="/home" className="nav-item nav-link text-light">
-            Home
-          </Link>
-
-          {/* Display the login and register button only if there is no user
+        {/* Display the login and register button only if there is no user
              that is currently logged in
           */}
 
-          {!this.props.isLoggedIn && (
-            <Link to="/login" className="nav-item nav-link text-light">
-              Login
-            </Link>
-          )}
+        {!props.isLoggedIn && (
+          <Link to="/login" className="nav-item nav-link text-light">
+            Login
+          </Link>
+        )}
 
-          {!this.props.isLoggedIn && (
-            <Link
-              to="/register"
-              className="nav-item nav-link text-light active"
-            >
-              Register
-            </Link>
-          )}
+        {!props.isLoggedIn && (
+          <Link to="/register" className="nav-item nav-link text-light active">
+            Register
+          </Link>
+        )}
 
-          {/* Display the logout button only if there is a user that is currently
+        {/* Display the logout button only if there is a user that is currently
           logged in
           */}
-          {this.props.isLoggedIn && (
-            <Link to="/addTransaction" className="nav-item nav-link text-light">
-              Add Transaction
-            </Link>
-          )}
+        {props.isLoggedIn && (
+          <Link to="/addTransaction" className="nav-item nav-link text-light">
+            Add Transaction
+          </Link>
+        )}
 
-          {this.props.isLoggedIn && (
-            <Link
-              to="/logout"
-              className="nav-item nav-link text-light"
-              onClick={() => this.onLogoutClick()}
-            >
-              Logout
-            </Link>
-          )}
-        </Navbar>
-      </div>
-    );
-  }
-}
+        {props.isLoggedIn && (
+          <Link
+            to="/logout"
+            className="nav-item nav-link text-light"
+            onClick={() => props.handleLogout()}
+          >
+            Logout
+          </Link>
+        )}
+      </Navbar>
+    </div>
+  );
+};
 
 // PropTypes is one way of adding type checking to JavaScript. Other
 // competing projects are TypeScript from Microsoft and Flow from Facebook
@@ -73,3 +63,5 @@ export default class NavBar extends Component {
 NavBar.propTypes = {
   isLoggedIn: PropTypes.bool.isRequired,
 };
+
+export default NavBar;
