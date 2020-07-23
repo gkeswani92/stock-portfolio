d446c5ad2be9d5351ff188fd9560857b1a6637ab
gkeswani92 <gauravkeswani92@gmail.com>
2020-07-22 22:54:02 -0700

    Add schema for ticker in schema.sql

--- server/schema.sql
+++ server/schema.sql
@@ -6,3 +6,14 @@ CREATE TABLE stock_exchange (
     PRIMARY KEY (id),
     CONSTRAINT unique_exchange UNIQUE(name)
 );
+
+
+CREATE TABLE ticker (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    exchange_id INT(1) NOT NULL,
+    ticker VARCHAR(64) NOT NULL,
+    company_name VARCHAR(512) NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT unique_ticker_exchange UNIQUE(exchange_id, ticker),
+    FOREIGN KEY (exchange_id) REFERENCES stock_exchange(id)
+);
